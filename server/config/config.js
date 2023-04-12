4b23815116626b2037a22819c7055efa4f821d99
Denis Herrera Espiritu <jsherr.6@gmail.com>
2023-04-11 21:35:18 -0500

    mongodb_uri

--- server/config/config.js
+++ server/config/config.js
@@ -9,4 +9,4 @@ export const CLOUD_KEY = process.env.CLOUDINARY_API_KEY
 export const CLOUD_SECRET = process.env.CLOUDINARY_API_SECRET
 
 export const PORT = process.env.PORT
-export const URL = process.env.MONGODB_URI
+// export const URL = process.env.MONGODB_URI
--- server/config/connectDB.js
+++ server/config/connectDB.js
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose'
-import { URL } from './config.js'
+// import { URL } from './config.js'
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(URL)
+    const conn = await mongoose.connect(process.env.MONGODB_URI)
     console.log(`MongoDB Connected: ${conn.connection.host}`)
   } catch (err) {
     throw err.message
